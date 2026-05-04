49aa798040ba75184f41b7982c62b44329c277bf
shamirshehbazkhan-html <shamirshehbazkhan@gmail.com>
2026-05-04 16:45:45 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Abhi JS zaroori nahi — future ke liye
+console.log("iPhone Table Loaded");
+function openChat() {
+  document.getElementById("chatBox").style.display = "block";
+}
+
+function closeChat() {
+  document.getElementById("chatBox").style.display = "none";
+}
